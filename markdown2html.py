1858248157e348eec0fe17aad1b139b37a7329ca
sevajal <sevajal@yahoo.com>
2022-12-15 22:11:27 -0500

    I did the task 1

--- markdown2html.py
+++ markdown2html.py
@@ -9,6 +9,7 @@ Second argument is the output file name
 if __name__ == "__main__":
     import sys
     import os
+    import markdown
     args = len(sys.argv)
     if args < 2:
         print(f"Usage: ./markdown2html.py README.md README.html")
@@ -17,4 +18,8 @@ if __name__ == "__main__":
         if not os.path.exists(sys.argv[1]):
             print(f"Missing {sys.argv[1]}")
             exit(1)
+        with open(sys.argv[1], "r") as f:
+            markdown = markdown.markdown(f.read())
+        with open(sys.argv[2], "w") as g:
+            g.write(markdown)
         exit(0)
